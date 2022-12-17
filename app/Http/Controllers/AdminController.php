3dc0b9aaaa889721a6342803b5ebe1190c6a3644
musfiqur521 <musfiqur521@gmail.com>
2022-12-18 04:13:37 +0600

    Database Login

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function addview()
+    {
+        return view('admin.add_doctor');
+    }
+
+    public function upload(Request $request)
+    {
+        $doctor=new doctor;
+        $image=$request->file;
+        $imagename=time().'.'.$image->getClientoriginalExtension();
+        $request->file->move('doctorimage',$imagename);
+        $doctor->image=$imagename;
+
+        $doctor->name=$request->name;
+        $doctor->phone=$request->number;
+        $doctor->room=$request->room;
+        $doctor->specialty=$request->specialty;
+
+        $doctor->save();
+        return redirect()->back()->with('message','Doctor Added Successfully');
+    }
+
+}
